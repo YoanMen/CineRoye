9c16ef00e039d11d19e64676d7fcde966d78cd3e
YoanMen <yoan.menard.dev@gmail.com>
2023-08-14 16:59:00 +0200

    Correction of a ComingSoon page

--- lib/features/movie_showing/comingSoon/coming_soon_screen.dart
+++ lib/features/movie_showing/comingSoon/coming_soon_screen.dart
@@ -5,74 +5,62 @@ import 'package:cineroye/features/movie_showing/onDisplay/movie_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../theme/palette.dart';
+
 class ComingSoonScreen extends ConsumerWidget {
   const ComingSoonScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    var isPhone = MediaQuery.of(context).size.width < 512;
-
     final theme = Theme.of(context);
     final moviesData = ref.watch(movieShowingControllerProvider).moviesSoon;
 
     return moviesData.when(
-      loading: () => const Center(
-        child: CircularProgressIndicator(),
-      ),
-      error: (error, stackTrace) => ErrorMessage(
-          onRefresh:
-              ref.read(movieShowingControllerProvider.notifier).fetchComingSoon,
-          error: error),
-      data: (data) {
-        return data.isEmpty
-            ? ErrorMessage(
-                onRefresh: ref
-                    .read(movieShowingControllerProvider.notifier)
-                    .fetchComingSoon,
-                error: "Aucun film n'est programmé pour le moment",
-              )
-            : Scaffold(
-                body: SafeArea(
-                  child: SingleChildScrollView(
-                    physics: const BouncingScrollPhysics(),
-                    child: Column(
-                      children: [
-                        const SizedBox(
-                          height: kmediumSpace,
+        loading: () => const Center(
+              child: CircularProgressIndicator(),
+            ),
+        error: (error, stackTrace) => ErrorMessage(
+            onRefresh: ref
+                .read(movieShowingControllerProvider.notifier)
+                .fetchComingSoon,
+            error: error),
+        data: (data) {
+          return data.isEmpty
+              ? ErrorMessage(
+                  onRefresh: ref
+                      .read(movieShowingControllerProvider.notifier)
+                      .fetchMovies,
+                  error: "Aucun films en projection",
+                )
+              : Scaffold(
+                  body: SafeArea(
+                  child: RefreshIndicator(
+                      backgroundColor: blackColor,
+                      onRefresh: ref
+                          .read(movieShowingControllerProvider.notifier)
+                          .fetchComingSoon,
+                      child: SingleChildScrollView(
+                        physics: const BouncingScrollPhysics(),
+                        child: Column(
+                          children: [
+                            const SizedBox(
+                              height: kmediumSpace,
+                            ),
+                            ListView.builder(
+                              physics: const NeverScrollableScrollPhysics(),
+                              shrinkWrap: true,
+                              itemCount: data.length,
+                              itemBuilder: (context, index) =>
+                                  MovieListAnimation(
+                                      movie: data[index], theme: theme),
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).padding.top + 20,
+                            ),
+                          ],
                         ),
-                        SizedBox(
-                          height: MediaQuery.of(context).padding.top,
-                        ),
-                        Expanded(
-                          child: isPhone
-                              ? ListView.builder(
-                                  itemCount: data.length,
-                                  itemBuilder: (context, index) =>
-                                      MovieListAnimation(
-                                          movie: data[index], theme: theme),
-                                )
-                              : GridView.builder(
-                                  gridDelegate:
-                                      const SliverGridDelegateWithFixedCrossAxisCount(
-                                          mainAxisExtent: 200,
-                                          crossAxisCount: 2),
-                                  itemCount: data.length,
-                                  itemBuilder: (context, index) =>
-                                      MovieListAnimation(
-                                    movie: data[index],
-                                    theme: theme,
-                                  ),
-                                ),
-                        ),
-                        SizedBox(
-                          height: MediaQuery.of(context).padding.top + 20,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              );
-      },
-    );
+                      )),
+                ));
+        });
   }
 }
